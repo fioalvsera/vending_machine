95ce1f5cbb06d965cf432644d5a24fc03bf5e4cf
fioalvsera <fioalvsera@gmail.com>
2021-10-17 08:17:27 -0600

    Adds loop to come back to the menu

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
 
 const coinSystem = require("./coin_system.js")
+const util = require("./util.js")
 
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-
-async function readLineAsync(message) {
-    return new Promise((resolve, reject) => {
-        readline.question(message, (answer) => {
-        resolve(answer);
-        });
-    });
-}
 
 const main = async () => {
 
+    let continueUsingVendingMachine = true
     let credit = 0
-    console.log("This is a Vending Machine")
-    console.log(`Your credit is ${credit}`)
-    console.log("Type '1' if you wish to add credit")
-    console.log("Type '2' if you wish to see the products")
-    let usersResponse = await readLineAsync("What do you wish to do?")
 
-    switch(usersResponse){
+    do {
+        console.log("This is a Vending Machine")
+        console.log(`Your credit is ${credit}`)
+        console.log("Type '1' if you wish to add credit")
+        console.log("Type '2' if you wish to see the products")
+        console.log("Type '3' if you wish to exit")
+        let usersResponse = await util.readLineAsync("What do you wish to do?")
 
-        case "1":
-            credit = await coinSystem.runCoinSystem(credit)
-            console.log(`Your new credit is ${credit}`)
-            break;
-        case "2":
-            console.log("This are our Products")
-            break;
+        switch(usersResponse){
 
-        default:
-            console.log("error")
-    }
+            case "1":
+                credit = credit + await coinSystem.runCoinSystem(credit)
+                console.log(`Your new credit is ${credit}`)
+                break;
+            case "2":
+                console.log("This are our Products")
+                break;
+
+            case "3":
+                continueUsingVendingMachine = false
+                break;
+
+            default:
+                console.log("error")
+        }
+    } while (continueUsingVendingMachine)
 
     process.exit(0)
 
